98be5602fdd80d9176a496366250ed58879e3310
Valerian Saliou <valerian@valeriansaliou.name>
2022-02-04 16:21:08 +0100

    Add base types

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -3,4 +3,24 @@
 // Copyright: 2022, Valerian Saliou <valerian@valeriansaliou.name>
 // License: Mozilla Public License v2.0 (MPL v2.0)
 
-// TODO
+use jid::BareJid;
+use xmpp_parsers;
+use tokio_xmpp;
+
+// -- Structures --
+
+pub enum ProseClientOrigin {
+    TestsCLI,
+    ProseAppMacOS,
+    ProseAppIOS,
+    ProseAppAndroid,
+    ProseAppWindows,
+    ProseAppLinux,
+    ProseAppWeb,
+}
+
+pub struct ProseClient {
+    pub jid: BareJid,
+    pub password: String,
+    pub origin: ProseClientOrigin
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,9 @@
 // Copyright: 2022, Valerian Saliou <valerian@valeriansaliou.name>
 // License: Mozilla Public License v2.0 (MPL v2.0)
 
+mod broker;
+mod client;
+mod store;
+mod types;
+
 // TODO
