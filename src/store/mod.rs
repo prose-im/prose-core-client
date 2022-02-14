e41896ed13d1352d7246cad1fa857576fc11d68b
Valerian Saliou <valerian@valeriansaliou.name>
2022-02-14 10:57:35 +0100

    Rack all models

--- src/store/mod.rs
+++ src/store/mod.rs
@@ -3,6 +3,10 @@
 // Copyright: 2022, Valerian Saliou <valerian@valeriansaliou.name>
 // License: Mozilla Public License v2.0 (MPL v2.0)
 
+// -- Modules --
+
+mod models;
+
 // TODO
 // Data Store / Modeling Crates (Pure-Rust required!)
 //   - mdl: https://docs.rs/mdl/latest/mdl/ (perfect fit? has signals for updated data, cool!)
---
+++ src/store/models/mod.rs
@@ -0,0 +1,13 @@
+// prose-core-client
+//
+// Copyright: 2022, Valerian Saliou <valerian@valeriansaliou.name>
+// License: Mozilla Public License v2.0 (MPL v2.0)
+
+// -- Modules --
+
+mod accounts;
+mod feeds;
+mod messages;
+mod profiles;
+mod security;
+mod settings;
