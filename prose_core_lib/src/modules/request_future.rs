21ac5e7f2f435143ef134e6bcbe8616ca5e1b071
mb <mb@nesium.com>
2023-06-14 17:23:01 +0200

    feat: Improve parsing of XMPP errors

--- prose_core_lib/src/modules/request_future.rs
+++ prose_core_lib/src/modules/request_future.rs
@@ -1,9 +1,12 @@
+use std::fmt::{Display, Formatter};
 use std::future::Future;
 use std::pin::Pin;
 use std::sync::{Arc, Mutex};
 use std::task::{Context, Poll, Waker};
 
-use crate::stanza::{StanzaBase, IQ};
+use strum_macros::{Display, EnumString};
+
+use crate::stanza::{Stanza, StanzaBase, IQ};
 
 #[derive(Debug, thiserror::Error)]
 pub enum RequestError {
@@ -13,6 +16,78 @@ pub enum RequestError {
     TimedOut,
     #[error("request error: Unexpected server response")]
     UnexpectedResponse,
+    #[error("XMPP error: {err}")]
+    XMPP { err: XMPPError },
+}
+
+#[derive(Debug)]
+pub struct XMPPError {
+    pub kind: Option<XMPPErrorKind>,
+    pub text: Option<String>,
+    pub status: Option<XMPPErrorStatus>,
+}
+
+impl Display for XMPPError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("{:?}", self))
+    }
+}
+
+#[derive(Debug, PartialEq, Display, EnumString, Clone)]
+#[strum(serialize_all = "lowercase")]
+pub enum XMPPErrorKind {
+    Auth,
+    Cancel,
+    Modify,
+    Wait,
+}
+
+#[derive(Debug, PartialEq, Display, EnumString, Clone)]
+pub enum XMPPErrorStatus {
+    #[strum(serialize = "bad-request")]
+    BadRequest,
+    #[strum(serialize = "conflict")]
+    Conflict,
+    #[strum(serialize = "feature-not-implemented")]
+    FeatureNotImplemented,
+    #[strum(serialize = "forbidden")]
+    Forbidden,
+    #[strum(serialize = "gone")]
+    Gone,
+    #[strum(serialize = "internal-server-error")]
+    InternalServerError,
+    #[strum(serialize = "item-not-found")]
+    ItemNotFound,
+    #[strum(serialize = "jid-malformed")]
+    JidMalformed,
+    #[strum(serialize = "not-acceptable")]
+    NotAcceptable,
+    #[strum(serialize = "not-allowed")]
+    NotAllowed,
+    #[strum(serialize = "not-authorized")]
+    NotAuthorized,
+    #[strum(serialize = "payment-required")]
+    PaymentRequired,
+    #[strum(serialize = "recipient-unavailable")]
+    RecipientUnavailable,
+    #[strum(serialize = "redirect")]
+    Redirect,
+    #[strum(serialize = "registration-required")]
+    RegistrationRequired,
+    #[strum(serialize = "remote-server-not-found")]
+    RemoteServerNotFound,
+    #[strum(serialize = "remote-server-timeout")]
+    RemoteServerTimeout,
+    #[strum(serialize = "resource-constraint")]
+    ResourceConstraint,
+    #[strum(serialize = "service-unavailable")]
+    ServiceUnavailable,
+    #[strum(serialize = "subscription-required")]
+    SubscriptionRequired,
+    #[strum(serialize = "undefined-condition")]
+    UndefinedCondition,
+    #[strum(serialize = "unexpected-request")]
+    UnexpectedRequest,
 }
 
 impl<'a> TryFrom<&IQ<'a>> for RequestError {
@@ -22,8 +97,30 @@ impl<'a> TryFrom<&IQ<'a>> for RequestError {
         let Some(error) = stanza.child_by_name("error") else {
             return Err(())
         };
+
+        if let Ok(xmpp_err) = XMPPError::try_from(&error) {
+            return Ok(RequestError::XMPP { err: xmpp_err });
+        }
+
         Ok(RequestError::Generic {
             msg: error.to_string(),
+        })
+    }
+}
+
+impl<'a> TryFrom<&Stanza<'a>> for XMPPError {
+    type Error = ();
+
+    fn try_from(stanza: &Stanza<'a>) -> Result<Self, Self::Error> {
+        Ok(XMPPError {
+            kind: stanza
+                .attribute("type")
+                .and_then(|attr| attr.parse::<XMPPErrorKind>().ok()),
+            text: stanza.child_by_name("text").and_then(|s| s.text()),
+            status: stanza
+                .first_child()
+                .and_then(|s| s.name().map(ToOwned::to_owned))
+                .and_then(|s| s.parse::<XMPPErrorStatus>().ok()),
         })
     }
 }
