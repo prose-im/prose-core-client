a9b495344a399307e352545ab8292f2464d5e4b4
Valerian Saliou <valerian@valeriansaliou.name>
2023-08-18 14:12:59 +0200

    fix: make capabilities debuggable

--- crates/prose-core-client/src/types/capabilities.rs
+++ crates/prose-core-client/src/types/capabilities.rs
@@ -16,7 +16,8 @@ pub struct Capabilities {
     pub node: String,
     pub identity: Identity,
     pub features: Vec<Feature>,
-    pub sha1_ver_hash: String,
+    pub ver_string: String,
+    pub ver_hash: String,
 }
 
 impl Capabilities {
@@ -29,16 +30,19 @@ impl Capabilities {
             category: "client".to_string(),
             kind: "pc".to_string(),
             name: client_name.into(),
+            lang: "en".to_string(),
         };
         let features: Vec<Feature> = features.into_iter().collect();
 
-        let sha1_ver_hash = Capabilities::sha1_ver_hash(&identity, features.iter());
+        let ver_string = Capabilities::ver_string(&identity, features.iter());
+        let ver_hash = Capabilities::ver_hash(&ver_string);
 
         Capabilities {
             node: client_website.into(),
             identity,
             features,
-            sha1_ver_hash,
+            ver_string,
+            ver_hash,
         }
     }
 }
@@ -48,6 +52,7 @@ pub struct Identity {
     pub category: String,
     pub kind: String,
     pub name: String,
+    pub lang: String,
 }
 
 #[derive(Clone, Debug)]
@@ -70,29 +75,29 @@ impl Display for Feature {
 }
 
 impl Capabilities {
-    fn sha1_ver_hash<'a>(
+    fn ver_string<'a>(
         identity: &Identity,
         features: impl Iterator<Item = &'a Feature>,
     ) -> String {
-        let mut str = format!(
+        let mut string = format!(
             "{}/{}/{}/{}<",
-            identity.category, identity.kind, "", identity.name
-        )
-        .into_bytes();
+            identity.category, identity.kind, identity.lang, identity.name
+        );
 
-        let mut features: Vec<Vec<u8>> = features
-            .into_iter()
-            .map(|f| f.to_string().into_bytes())
-            .collect();
+        let mut features: Vec<String> = features.into_iter().map(|f| f.to_string()).collect();
         features.sort();
 
         for feat in features {
-            str.extend(feat);
-            str.extend(b"<");
+            string.push_str(&feat);
+            string.push_str("<");
         }
 
+        string
+    }
+
+    fn ver_hash(ver_string: &str) -> String {
         let mut hasher = Sha1::new();
-        hasher.update(str);
+        hasher.update(ver_string.as_bytes());
         general_purpose::STANDARD.encode(hasher.finalize())
     }
 }
@@ -100,7 +105,7 @@ impl Capabilities {
 impl From<&Capabilities> for xmpp_parsers::disco::DiscoInfoResult {
     fn from(value: &Capabilities) -> Self {
         xmpp_parsers::disco::DiscoInfoResult {
-            node: Some(format!("{}#{}", value.node, value.sha1_ver_hash)),
+            node: Some(format!("{}#{}", value.node, value.ver_hash)),
             identities: vec![(&value.identity).into()],
             features: value.features.iter().map(Into::into).collect(),
             extensions: vec![],
@@ -141,15 +146,15 @@ impl From<&Feature> for xmpp_parsers::disco::Feature {
 
 #[cfg(test)]
 mod tests {
-    use xmpp_parsers::ns;
+    use prose_xmpp::ns;
 
     use super::*;
 
     #[test]
-    fn test_ver_hash() {
+    fn test_ver_hash_exodus() {
         let caps = Capabilities::new(
             "Exodus 0.9.1",
-            "",
+            "http://code.google.com/p/exodus",
             vec![
                 Feature::Name(ns::MUC),
                 Feature::Name(ns::CAPS),
@@ -158,6 +163,50 @@ mod tests {
             ],
         );
 
-        assert_eq!(caps.sha1_ver_hash, "QgayPKawpkPSDYmwT/WM94uAlu0=");
+        assert_eq!(caps.ver_string, "client/pc/en/Exodus 0.9.1<http://jabber.org/protocol/caps<http://jabber.org/protocol/disco#info<http://jabber.org/protocol/disco#items<http://jabber.org/protocol/muc<");
+        assert_eq!(caps.ver_hash, "P0yYsh3UL9xwD/EPgkK6u5Fsa5A=");
+    }
+
+    #[test]
+    fn test_ver_hash_prose() {
+        let caps = Capabilities::new(
+            "Prose",
+            "https://prose.org",
+            vec![
+                Feature::Name(ns::JABBER_CLIENT),
+                Feature::Name(ns::AVATAR_DATA),
+                Feature::Name(ns::AVATAR_METADATA),
+                Feature::Name(ns::CHATSTATES),
+                Feature::Name(ns::DISCO_INFO),
+                Feature::Name(ns::RSM),
+                Feature::Name(ns::CAPS),
+                Feature::Name(ns::PING),
+                Feature::Name(ns::PUBSUB),
+                Feature::Name(ns::PUBSUB_EVENT),
+                Feature::Name(ns::ROSTER),
+                Feature::Name(ns::REACTIONS),
+                Feature::Name(ns::RECEIPTS),
+                Feature::Name(ns::CHAT_MARKERS),
+                Feature::Name(ns::MESSAGE_CORRECT),
+                Feature::Name(ns::RETRACT),
+                Feature::Name(ns::FASTEN),
+                Feature::Name(ns::DELAY),
+                Feature::Name(ns::FALLBACK),
+                Feature::Name(ns::HINTS),
+                Feature::Name(ns::MAM),
+                Feature::Name(ns::TIME),
+                Feature::Name(ns::VERSION),
+                Feature::Name(ns::LAST_ACTIVITY),
+                Feature::Name(ns::USER_ACTIVITY),
+                Feature::Name(ns::VCARD4),
+                Feature::Notify(ns::PUBSUB),
+                Feature::Notify(ns::USER_ACTIVITY),
+                Feature::Notify(ns::AVATAR_METADATA),
+                Feature::Notify(ns::VCARD4),
+            ],
+        );
+
+        assert_eq!(caps.ver_string, "client/pc/en/Prose<http://jabber.org/protocol/activity<http://jabber.org/protocol/activity+notify<http://jabber.org/protocol/caps<http://jabber.org/protocol/chatstates<http://jabber.org/protocol/disco#info<http://jabber.org/protocol/pubsub<http://jabber.org/protocol/pubsub#event<http://jabber.org/protocol/pubsub+notify<http://jabber.org/protocol/rsm<jabber:client<jabber:iq:last<jabber:iq:roster<jabber:iq:version<urn:ietf:params:xml:ns:vcard-4.0<urn:ietf:params:xml:ns:vcard-4.0+notify<urn:xmpp:avatar:data<urn:xmpp:avatar:metadata<urn:xmpp:avatar:metadata+notify<urn:xmpp:chat-markers:0<urn:xmpp:delay<urn:xmpp:fallback:0<urn:xmpp:fasten:0<urn:xmpp:hints<urn:xmpp:mam:2<urn:xmpp:message-correct:0<urn:xmpp:message-retract:0<urn:xmpp:ping<urn:xmpp:reactions:0<urn:xmpp:receipts<urn:xmpp:time<");
+        assert_eq!(caps.ver_hash, "Oslk/IqOlraX9ZosbeRbqA0IegA=");
     }
 }
