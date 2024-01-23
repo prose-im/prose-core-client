410148ab131445f4d1fe50604f6c6c01e493d43a
mb <mb@nesium.com>
2024-01-23 22:49:03 +0100

    chore: Update uniffi, disable cargo-swift temporarily due to incompatible dependencies

--- xtask/src/swift.rs
+++ xtask/src/swift.rs
@@ -1,8 +1,8 @@
 use std::env;
 
 use anyhow::Result;
-use cargo_swift::package::{run, LibTypeArg, Platform};
-use cargo_swift::{Config, Mode};
+// use cargo_swift::package::{run, LibTypeArg, Platform};
+// use cargo_swift::{Config, Mode};
 
 #[derive(clap::Args)]
 pub struct Args {
@@ -17,21 +17,22 @@ enum Command {
 
 impl Args {
     pub async fn run(self) -> Result<()> {
-        env::set_current_dir("bindings/prose-sdk-ffi")?;
-
-        run(
-            Some(vec![Platform::Macos]),
-            Some("ProseSDK".to_string()),
-            false,
-            Config {
-                silent: false,
-                accept_all: false,
-            },
-            Mode::Debug,
-            LibTypeArg::Static,
-            false,
-        )?;
-
-        Ok(())
+        todo!("FIXME")
+        // env::set_current_dir("bindings/prose-sdk-ffi")?;
+        //
+        // run(
+        //     Some(vec![Platform::Macos]),
+        //     Some("ProseSDK".to_string()),
+        //     false,
+        //     Config {
+        //         silent: false,
+        //         accept_all: false,
+        //     },
+        //     Mode::Debug,
+        //     LibTypeArg::Static,
+        //     false,
+        // )?;
+        //
+        // Ok(())
     }
 }
